c2fca634480b24a1b2c4cadfd7220ac13965836d
Maria Hayward <hayward.maria@gmail.com>
2021-05-28 15:04:21 -0300

    subo avances del search

--- src/Components/ButtonListSearch/ButtonListSearch.js
+++ src/Components/ButtonListSearch/ButtonListSearch.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import data from "../Communities/data";
 
 const ButtonListSearch = () => {
 
-	const { communities } = this.state;
-	console.log(communities)
 	return (
 		<ul className="movies__list">
-			{communities.map((u) => (
+			{data.map((u) => (
 				<Button coms={u} />
 				// pasarle la info aca a las peliculas y quye estas se las pasen el modal?
 			))}
